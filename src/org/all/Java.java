0ddbcb6c78b2b952b8e52d821973db65a336b3f6
VaishuKeerthi <dazzlingvaishu1406@gmail.com>
2023-07-24 09:59:37 +0530

    Added

---
+++ src/org/all/Java.java
@@ -0,0 +1,24 @@
+package org.all;
+
+public class Java {
+	
+	public static void main(String[] args) {
+	int Count =0;
+		
+		for(int i=1;i<=10;i++) {
+			
+			if(i % 2 == 0) {
+				
+				Count ++;
+				
+			}
+		}
+		
+		System.out.println(Count +" Count Even Number");
+	}
+
+}
+
+	
+
+
